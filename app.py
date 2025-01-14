1377081202989f615c9127a3e7f1bf0a1d57858a
conferencesked.com <93970468+conferencesked@users.noreply.github.com>
2025-01-14 13:48:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,16 +26,9 @@ def add_book():
     books.append(book)
     return jsonify(book)
 
-# Endpoint to get a book by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    for book in books:
-        if book['id'] == book_id:
-            return jsonify(book)
-    return '', 404
-    
+# Endpoint to get a book by ID  
 # New endpoint to process a URL within the books service
-@app.route('/reading-list/books/process-url/<path:url>', methods=['GET'])
+@app.route('/reading-list/books/<path:url>', methods=['GET'])
 def process_url(url):
     """
     Accepts a URL as a route parameter and processes it by
@@ -45,22 +38,5 @@ def process_url(url):
     processed_url = url.replace('www.', '').replace('.com', '')
     return jsonify({'processed_url': processed_url})
     
-# Endpoint to delete a book by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['DELETE'])
-def delete_book(book_id):
-    global books
-    books = [book for book in books if book['id'] != book_id]
-    return '', 204
-
-# Endpoint to update a book status by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['PUT'])
-def update_book_status(book_id):
-    data = request.get_json()
-    for book in books:
-        if book['id'] == book_id:
-            book['status'] = data['status']
-            return jsonify(book)
-    return '', 404
-
 if __name__ == '__main__':
     app.run()
