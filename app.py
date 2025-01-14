c1acfe69e1d5f830c1a49166c459a59f13ab1d3b
conferencesked.com <93970468+conferencesked@users.noreply.github.com>
2025-01-14 14:13:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,8 @@ def add_book():
     books.append(book)
     return jsonify(book)
 
-# Endpoint to get a book by ID  
-# New endpoint to process a URL within the books service
-@app.route('/reading-list/books/<path:url>', methods=['GET'])
+# Endpoint to process a URL within the books service
+@app.route('/reading-list/books/process-url/<path:url>', methods=['GET'])
 def process_url(url):
     """
     Accepts a URL as a route parameter and processes it by
@@ -37,6 +36,17 @@ def process_url(url):
     # Remove 'www.' and '.com' from the URL
     processed_url = url.replace('www.', '').replace('.com', '')
     return jsonify({'processed_url': processed_url})
+
+# Endpoint to get a book by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    """
+    Retrieves a book by its ID.
+    """
+    for book in books:
+        if book['id'] == book_id:
+            return jsonify(book)
+    return '', 404
     
 if __name__ == '__main__':
     app.run()
